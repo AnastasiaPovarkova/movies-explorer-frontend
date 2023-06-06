d3c7209817f95bdde6097a980bdb1c8c42c86081
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-06-06 15:35:34 +0300

    Доработаны роуты в Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,27 +8,18 @@ function Header() {
     <header className="header">
       <div className="header__left">
       <Routes>
-        <Route path="/" element={<img className="header__logo" src={logo} alt="Movies Explorer" />}></Route>
-        <Route path="/movies" element={<img className="header__logo" src={logo} alt="Movies Explorer" />}></Route>
-        <Route path="/saved-movies" element={<img className="header__logo" src={logo} alt="Movies Explorer" />}></Route>
-        <Route path="/profile" element={<img className="header__logo" src={logo} alt="Movies Explorer" />}></Route>
+        {["/", "/movies", "/saved-movies", "/profile"].map((path) => (
+          <Route path={path} element={<img className="header__logo" src={logo} alt="Movies Explorer" />} />
+        ))}
       </Routes>
         <div className="header__nav">
           <Routes>
-            <Route path="/" element={<> 
-            </>} />
-            <Route path="/movies" element={<> 
-              <Link to="/movies" className="header__navlink header__navlink-500">Фильмы</Link>
-              <Link to="/saved-movies" className="header__navlink">Сохранённые фильмы</Link>
-            </>} />
-            <Route path="/saved-movies" element={<> 
-              <Link to="/movies" className="header__navlink header__navlink-500">Фильмы</Link>
-              <Link to="/saved-movies" className="header__navlink">Сохранённые фильмы</Link>
-            </>} />
-            <Route path="/profile" element={<> 
-              <Link to="/movies" className="header__navlink header__navlink-500">Фильмы</Link>
-              <Link to="/saved-movies" className="header__navlink">Сохранённые фильмы</Link>
-            </>} />
+            {["/movies", "/saved-movies", "/profile"].map((path) => (
+              <Route path={path} element={<> 
+                <Link to="/movies" className="header__navlink header__navlink-500">Фильмы</Link>
+                <Link to="/saved-movies" className="header__navlink">Сохранённые фильмы</Link>
+              </>} />
+            ))}
           </Routes>
         </div>
       </div>
@@ -38,15 +29,9 @@ function Header() {
             <Link to="/signup" className="header__link">Регистрация</Link>
             <Link to="/signin" className="header__link header__link-entry">Войти</Link>
           </>} />
-          <Route path="/movies" element={<> 
-            <Link to="/profile" className="header__link header__link-account">Аккаунт</Link>
-          </>} />
-          <Route path="/saved-movies" element={<> 
-            <Link to="/profile" className="header__link header__link-account">Аккаунт</Link>
-          </>} />
-          <Route path="/profile" element={<> 
-            <Link to="/profile" className="header__link header__link-account">Аккаунт</Link>
-          </>} />
+          {["/movies", "/saved-movies", "/profile"].map((path) => (
+            <Route path={path} element={<Link to="/profile" className="header__link header__link-account">Аккаунт</Link>} />
+          ))}
         </Routes>
       </div>
     </header>
