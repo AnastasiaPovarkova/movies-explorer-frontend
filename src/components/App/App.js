14eeece43b36a10baad347547f0904fd5162c712
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-07-19 15:36:58 +0300

    Анимирован main, убрана минимальная длина поиска

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,6 +25,7 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [isAuthLoading, setIsAuthLoading] = useState(false);
   const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+  const [allMovies, setAllMovies] = useState([]);
   const [filterMovies, setFilterMovies] = useState([]);
   const [moviesForRender, setMoviesForRender] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -59,7 +60,13 @@ function App() {
 
   useEffect(() => {
     setIsPreloaderLoading(true);
-    if ((location.pathname === '/saved-movies')) {
+    if ((location.pathname === '/')) {
+      mainApi.getMovies() 
+      .then((allMovies) => {
+        setAllMovies(allMovies);
+      })
+      .catch((err) => console.log(err))
+    } else if ((location.pathname === '/saved-movies')) {
       moviesApi.getSavedMovies()
       .then((movies) => {
         setNothingFoundInSaved('');
@@ -371,14 +378,18 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Main />}
+            element={
+              <Main 
+                allMovies={allMovies}
+              />
+            }
           />
           <Route
             path="/movies"
             element={
               <ProtectedRoute 
                 element={Movies}
-                isLoading = {isLoading}
+                isLoading={isLoading}
                 filterMovies={moviesForRender}
                 onFilterCheckbox={handleFilterCheck}
                 onMovieSearch={handleMovieSearch} 
@@ -396,7 +407,7 @@ function App() {
             element={
               <ProtectedRoute 
                 element={SavedMovies}
-                isLoading = {isLoading}
+                isLoading={isLoading}
                 savedMovies={savedMovies}
                 onFilterCheckbox={handleSavedFilterCheck}
                 isChecked={isFilterCheckedInSaved}
@@ -413,7 +424,7 @@ function App() {
             element={
               <ProtectedRoute 
                 element={Profile}
-                isLoading = {isLoading}
+                isLoading={isLoading}
                 onLogout={handleLogout}
                 isEditing={isEditing}
                 setIsEditing={setIsEditing}
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,12 +1,43 @@
-import React from "react";
 import './Promo.css';
-import NavTab from '../NavTab/NavTab';
+// import NavTab from '../NavTab/NavTab';
 
-function Promo() {
+function Promo(props) {
+  console.log(props);
   return (
     <section className="promo">
-      <h2 className="promo__title">Учебный проект студента факультета Веб-разработки.</h2>
-      <NavTab />
+      <div className="promo__lenta">
+        <div className="promo__container"> 
+          <div className="promo__movies promo__movies-top">
+            {props.allMovies?.map((movie) => {
+              return (
+                <img src={`https://api.nomoreparties.co/${movie.image.url}`} 
+                    alt="movie" 
+                    className={`promo__movie ${(movie.id % 3 === 0) ? 'promo__movie-small' : ''} 
+                    ${((movie.id + 1) % 3 === 0) ? 'promo__movie-medium' : ''}
+                    ${((movie.id + 2) % 3 === 0) ? 'promo__movie-large' : ''}`}/>
+              );
+            })}
+          </div>
+        </div>
+        <div className="promo__container"> 
+          <div className="promo__movies">
+            {props.allMovies?.map((movie) => {
+              return (
+                <img src={`https://api.nomoreparties.co/${movie.image.url}`} 
+                    alt="movie" 
+                    className={`promo__movie ${(movie.id % 3 === 0) ? 'promo__movie-large' : ''} 
+                    ${((movie.id + 1) % 3 === 0) ? 'promo__movie-small' : ''}
+                    ${((movie.id + 2) % 3 === 0) ? 'promo__movie-medium' : ''}`}/>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="promo__info">
+        <h2 className="promo__text">Учебный проект студента факультета Веб-разработки</h2>
+        <h2 className="promo__title">Сервис для поиска фильмов</h2>
+      </div>
+      {/* <NavTab /> */}
     </section>
   );
 }
